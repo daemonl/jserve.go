53ee183c020fdf769ad756518b026235bb002b57
Damien Whitten <dw@chaos404.com>
2018-07-12 08:45:52 +1000

    More Middleware

--- middleware.go
+++ middleware.go
@@ -1,13 +1,47 @@
 package jserve
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"time"
+)
+
+var hostname string
+
+func init() {
+	hostname, _ = os.Hostname()
+}
 
 func VersionMiddleware(appName, version string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 			rw.Header().Add("X-Version", version)
 			rw.Header().Add("X-Application", appName)
+			rw.Header().Add("X-Hostname", hostname)
 			next.ServeHTTP(rw, req)
 		})
 	}
+}
+
+func UpHandler(appName, version string) http.Handler {
+	boot := time.Now()
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		json.NewEncoder(rw).Encode(map[string]interface{}{
+			"application": appName,
+			"version":     version,
+			"uptime":      time.Now().Sub(boot).Seconds(),
+			"hostname":    hostname,
+		})
+	})
+}
+
+func NotFoundHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		json.NewEncoder(rw).Encode(map[string]interface{}{
+			"status": "Not Found",
+			"path":   req.URL.Path,
+			"method": req.Method,
+		})
+	})
 }
